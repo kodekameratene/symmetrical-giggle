e62364212ad634af8b950c7ceb8125553a01b0f1
Henry Skorpe Sjøen <henryskorpe@gmail.com>
2019-08-04 20:39:37 +0000

    format document

--- run.it/src/pages/DocumentCreator.tsx
+++ run.it/src/pages/DocumentCreator.tsx
@@ -1,52 +1,52 @@
 import {
-  IonButtons,
-  IonCard,
-  IonCardContent,
-  IonCardHeader,
-  IonCardSubtitle,
-  IonCardTitle,
-  IonContent,
-  IonHeader,
-  IonIcon,
-  IonItem,
-  IonLabel,
-  IonList,
-  IonListHeader,
-  IonMenuButton,
-  IonTitle,
-  IonToolbar
-  } from '@ionic/react';
+    IonButtons,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonContent,
+    IonHeader,
+    IonIcon,
+    IonItem,
+    IonLabel,
+    IonList,
+    IonListHeader,
+    IonMenuButton,
+    IonTitle,
+    IonToolbar
+} from '@ionic/react';
 import { book, build, colorFill, grid } from 'ionicons/icons';
 import React from 'react';
 
-const DocumentCreator:React.FunctionComponent = () => {
-return (
-    <>
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonMenuButton />
-          </IonButtons>
-          <IonTitle>Home</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
- <IonCard class="welcome-card">
-          <img src="/assets/shapes.svg" alt=""/>
-          <IonCardHeader>
-            <IonCardSubtitle>An message from mr. kode</IonCardSubtitle>
-            <IonCardTitle>Some thoughts about Ionic</IonCardTitle>
-          </IonCardHeader>
-          <IonCardContent>
-            <p>
-              Things look pretty good and is easy to mock if we use the ionic-components.
+const DocumentCreator: React.FunctionComponent = () => {
+    return (
+        <>
+            <IonHeader>
+                <IonToolbar>
+                    <IonButtons slot="start">
+                        <IonMenuButton />
+                    </IonButtons>
+                    <IonTitle>DocumentCreator Page</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonCard class="welcome-card">
+                    <img src="/assets/shapes.svg" alt="" />
+                    <IonCardHeader>
+                        <IonCardSubtitle>An message from mr. kode</IonCardSubtitle>
+                        <IonCardTitle>Some thoughts about Ionic</IonCardTitle>
+                    </IonCardHeader>
+                    <IonCardContent>
+                        <p>
+                            Things look pretty good and is easy to mock if we use the ionic-components.
             </p>
-          </IonCardContent>
-        </IonCard>
-      </IonContent>
+                    </IonCardContent>
+                </IonCard>
+            </IonContent>
 
-</>
-);
+        </>
+    );
 }
 
 export default DocumentCreator;
